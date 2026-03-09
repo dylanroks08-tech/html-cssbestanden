b3818d894b56de876cefc1de07cddbc242d0075e
dylanroks08-tech <dylanroks08@gmail.com>
2026-03-09 10:23:18 +0100

    update

--- opdracht6.1java/opmaak.js
+++
@@ -1,6 +0,0 @@
-function kleur_aanpassen() {
-    document.getElementById("robot").style.color = "red";
-    document.getElementById("robot").innerHTML = "inhoud aangepast";
-}
-
-kleur_aanpassen()
--- opdracht6.2java/opmaak2.js
+++
@@ -1,4 +0,0 @@
-function kleur_aanpassen() {
-    document.getElementById("robot").style.color = "lightgreen";
-    document.getElementById("robot").innerHTML = "sower lemon";
-}
