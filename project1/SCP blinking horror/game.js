fa71c04ec7d44071c2ca1adaca5a628cc9f20ddc
dylanroks08-tech <dylanroks08@gmail.com>
2026-06-02 11:19:57 +0200

    update

--- project1/SCP blinking horror/game.js
+++ project1/SCP blinking horror/game.js
@@ -1,15 +1,15 @@
 
 window.onload = function startgame()
 {
-  setTimeout(myFunction, 5000);
+  setTimeout(myFunction, 1000);
     function myFunction() {
         document.getElementById("sound2").style.display = "none";
-    setTimeout(myFunction, 36000);
+    setTimeout(myFunction, 1000);
     function myFunction() {
         document.getElementById("change").style.display = "none";
         document.getElementById("achter").style.display = "none";
         document.getElementById("sound").style.display = "none";
-    setTimeout (myFunction, 60000) 
+    setTimeout (myFunction, 1000) 
     function myFunction() {
         document.getElementById("door").style.display = "none";
         ;}
