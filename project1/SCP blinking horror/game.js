a5965f458e607a523b249de828091013172a68fe
dylanroks08-tech <dylanroks08@gmail.com>
2026-06-02 10:23:04 +0200

    update

--- project1/SCP blinking horror/game.js
+++ project1/SCP blinking horror/game.js
@@ -1,8 +1,18 @@
 
-
-function startGame() {
-    setTimeout(myFunction, 6000);
+window.onload = function startgame()
+{
+  setTimeout(myFunction, 5000);
     function myFunction() {
-        document.getElementById("change").innerHTML = "You are in a dark room, you see a door and a papper file on the floor";
-}
+        document.getElementById("sound2").style.display = "none";
+    setTimeout(myFunction, 36000);
+    function myFunction() {
+        document.getElementById("change").style.display = "none";
+        document.getElementById("achter").style.display = "none";
+        document.getElementById("sound").style.display = "none";
+    setTimeout (myFunction, 60000) 
+    function myFunction() {
+        document.getElementById("door").style.display = "none";
+        ;}
+      }
+    }
 }
