91b70fb7efeeeba54a6ad440b8556faceedc7ffd
dylanroks08-tech <dylanroks08@gmail.com>
2026-06-05 11:16:12 +0200

    update

--- project1/SCP blinking horror/game.js
+++ project1/SCP blinking horror/game.js
@@ -1,18 +1,45 @@
 
 window.onload = function startgame()
 {
-  setTimeout(myFunction, 1000);
+  setTimeout(myFunction, 5000);
     function myFunction() {
         document.getElementById("sound2").style.display = "none";
-    setTimeout(myFunction, 1000);
+    setTimeout(myFunction, 40000);
     function myFunction() {
         document.getElementById("change").style.display = "none";
         document.getElementById("achter").style.display = "none";
         document.getElementById("sound").style.display = "none";
-    setTimeout (myFunction, 1000) 
+    setTimeout (myFunction, 60000) 
     function myFunction() {
         document.getElementById("door").style.display = "none";
-        ;}
-      }
+    setTimeout (myFunction, 24000) 
+    function myFunction() {
+        document.getElementById("peanut").style.display = "block";
+    setTimeout (myFunction, 24000)
+    function myFunction() {
+        document.getElementById("peanut").style.display = "none";
+        document.getElementById("peanut2").style.display = "block";
+    setTimeout (myFunction, 24000)
+    function myFunction() {
+        document.getElementById("peanut2").style.display = "none";
+        document.getElementById("peanut3").style.display = "block";
+    setTimeout (myFunction, 24000)
+    function myFunction() {
+        document.getElementById("peanut3").style.display = "none";
+        document.getElementById("peanut4").style.display = "block";
+    setTimeout (myFunction, 24000)
+    function myFunction() {
+        document.getElementById("peanut4").style.display = "none";
+        document.getElementById("peanut5").style.display = "block";
+    }
+    }
+    }
+    }
+    }
+    }
     }
 }
+}
+    
+
+
